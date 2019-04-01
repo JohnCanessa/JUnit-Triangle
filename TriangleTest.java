f5f914bf9b1f52f5d0af71342f081e5eeda6c223
John Canessa <8019504+JohnCanessa@users.noreply.github.com>
2019-04-01 06:59:52 -0500

    Add files via upload
    Added new test cases for a set of triangles specified by a test file.

--- TriangleTest.java
+++ TriangleTest.java
@@ -10,18 +10,22 @@ import junit.test.sample.Triangle.TriangleTypes;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 /*
  * 
  */
 public class TriangleTest {
 
 	// **** constructor ****
-	
 	public TriangleTest() {
 	}
 	
 	// **** ****
-	
 	@BeforeClass
 	public static void setUpClass() {
 		System.out.println("<<< setUpClass ...");
@@ -43,7 +47,6 @@ public class TriangleTest {
 	}
 	
 	// **** tests ****
-	
 	@Test
 	public void testScalene() {
 		System.out.println("<<< testScalene ...");
@@ -72,8 +75,91 @@ public class TriangleTest {
 		Triangle t = new Triangle(10, 5, 10);
 		String type = t.triangleType();
 		System.out.println("<<<     type ==>" + type + "<===");
-		String expected = TriangleTypes.Scalene.name();
+		String expected = TriangleTypes.Isosceles.name();
 		System.out.println("<<< expected ==>" + expected + "<==");
 		assertEquals(type, expected);
 	}
+	
+	// **** tests to read n triangles from file ****
+	@Test
+	public void testTriangles() {
+		
+		// **** ****
+		System.out.println("<<< testTriangles ...");
+
+		// **** open file with test cases (one per line) ****
+		BufferedReader br = null;
+		try {
+			br = new BufferedReader(new FileReader("c:\\Temp\\triangles.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// **** read count of triangles ****
+		String line = null;
+		try {
+			line = br.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("<<< line ==>" + line + "<==");
+		
+		// **** convert to integer ****
+		int n = Integer.parseInt(line);
+		System.out.println("<<< count: " + n);
+
+		// **** loop once per triangle ****
+		for (int i = 0; i < n; i++) {
+
+			// **** read sides and triangle type ****
+			try {
+				line = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("<<< line ==>" + line + "<==");
+		
+			// **** split the line ****
+			String s[] = line.split(" ");
+			
+			// **** extract triangle sides ****
+			int a = Integer.parseInt(s[0]);			
+			int b = Integer.parseInt(s[1]);
+			int c = Integer.parseInt(s[2]);
+			System.out.println("<<< a: " + a + " b: " + b + " c: " + c);
+			
+			// **** extract triangle type ****
+			String expected = null;
+			switch (s[3]) {
+			case "I":
+				expected = TriangleTypes.Isosceles.name();
+				break;
+			case "E":
+				expected = TriangleTypes.Equilateral.name();
+				break;
+			case "S":
+				expected = TriangleTypes.Scalene.name();
+				break;
+			}
+			System.out.println("<<< expected ==>" + expected + "<===");
+			
+			// **** instantiate triangle****
+			Triangle t = new Triangle(a, b, c);
+
+			// **** compute the triangle type ****
+			String type = t.triangleType();
+			System.out.println("<<<     type ==>" + type + "<===");
+			
+			// **** ****
+			assertEquals(type, expected);
+		}
+
+		// **** close file ****
+		try {
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+		
 }
